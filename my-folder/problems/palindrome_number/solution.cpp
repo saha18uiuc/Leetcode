77cc7415067c986d6ba3accb1f54dd832043b0c2
Dibyadeep Saha <saha18@illinois.edu>
2025-06-07 23:39:06 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

--- my-folder/problems/palindrome_number/solution.cpp
+++ my-folder/problems/palindrome_number/solution.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string x_str = to_string(x);
-        for (int i = 0, j = x_str.size() - 1; i < x_str.size(), j >= 0; i++, j--) {
-            if (x_str[i] != x_str[j]) {
+        string x_string = to_string(x);
+        for (int i = 0, j = x_string.size() - 1; i < x_string.size(), j >= 0; i++, j--) {
+            if (x_string[i] != x_string[j]) {
                 return false;
             }
         }
